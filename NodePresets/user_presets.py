c7ce4135ff95109e48bbbe62b5196f2cc72b7ddc
Derek Rein <derek.visualeffects@gmail.com>
2024-09-24 15:59:50 -0400

    add uv

--- NodePresets/user_presets.py
+++ NodePresets/user_presets.py
@@ -9,5 +9,6 @@ def nodePresetsStartup():
   nuke.setUserPreset("Expression", "FRINGE", {'expr3': '4*(1-a)*a', 'selected': 'true', 'label': '[knob expr3]'})
   nuke.setUserPreset("Expression", "FIX_EXP", {'channel3': 'alpha', 'temp_name0': 'z', 'selected': 'true', 'expr2': '(b < 0.00001 && b > 0) ? z : b', 'expr0': '(r < 0.00001 && r > 0)  ? z : r', 'expr1': '(g < 0.00001 && g > 0) ? z : g', 'temp_expr0': '0.00001 '})
   nuke.setUserPreset("Expression", "COLOR", {'expr2': '(1/(r*.3+g*.59+b*.11))*b', 'expr0': '(1/(r*.3+g*.59+b*.11))*r', 'expr1': '(1/(r*.3+g*.59+b*.11))*g', 'selected': 'true'})
+  nuke.setUserPreset("Expression", "UV", {'expr0': '(x + .5) / width', 'expr1': '(y + .5) / height', 'label': 'UV', 'selected': 'true'})
   nuke.setUserPreset("Expression", "NOT_BLACK", {'expr3': 'r + g + b == 0 ? 0 : 1', 'selected': 'true'})
   nuke.setUserPreset("Remove", "KEEP_RGBA", {'channels': 'rgba', 'operation': 'keep'})
